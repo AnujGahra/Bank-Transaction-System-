f5b32e5070e1b79b95138968e789296321172b24
Anuj Kumar <anujgahra244304@gmail.com>
2026-02-18 15:19:05 +0530

    User Login Route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,14 @@
 const express = require('express');
-const { userRegister } = require('../controllers/auth.controller');
+const { userRegister, userLogin } = require('../controllers/auth.controller');
 
 
 const router = express.Router();
 
 /* POST /api/auth/register */
 router.post('/register', userRegister);
+
+// POST /api/auth/login
+router.post('/login', userLogin);
 
 
 
