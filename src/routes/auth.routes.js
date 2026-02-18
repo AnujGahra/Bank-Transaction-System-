9667d5d792d5080ab58b5ef40708414b7f443f3d
Anuj Kumar <anujgahra244304@gmail.com>
2026-02-18 13:15:56 +0530

    User Register Route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const { userRegister } = require('../controllers/auth.controller');
 
 
 const router = express.Router();
 
+/* POST /api/auth/register */
+router.post('/register', userRegister);
 
 
 
 
 
 
-
-modele.exports = router;
+module.exports = router;
