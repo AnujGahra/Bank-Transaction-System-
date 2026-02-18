e72cb9c8b8555640548924546df42946b8ef30e3
Anuj Kumar <anujgahra244304@gmail.com>
2026-02-18 13:15:09 +0530

    middleware

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const authRouter = require('./routes/auth.routes');
+const cookieParser = require('cookie-parser');
 
 
 
 const app = express();
+
+app.use(express.json());  // It is used to parse incoming JSON request bodies and make the data available in req.body.
+app.use(cookieParser());  // It is used to parse cookies from the incoming HTTP requests and make them available in req.cookies.
 
 app.use("/api/auth", authRouter);
 
