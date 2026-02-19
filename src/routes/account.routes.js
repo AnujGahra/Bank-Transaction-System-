7d4a34eaf84f44a2cf21489707a1409286fc607b
Anuj Kumar <anujgahra244304@gmail.com>
2026-02-19 11:35:35 +0530

    Routes

--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const { authMiddleware } = require('../middleware/auth.middleware');
+const { createAccount } = require('../controllers/account.controller');
+
+
+
+
+
 const router = express.Router();
 
+
+/**
+ * - create account controller
+ * - POST /api/accounts/create
+ * - Protected Route, requires authentication
+ */
+
+router.post("/", authMiddleware, createAccount);
 
 
 
