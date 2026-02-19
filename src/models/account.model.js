51228544f4f5f6d537f4874926bd643438657544
Anuj Kumar <anujgahra244304@gmail.com>
2026-02-19 11:36:08 +0530

    Account Model

--- src/models/account.model.js
+++ src/models/account.model.js
@@ -1,28 +1,35 @@
 const mongoose = require('mongoose');
 
-const accountSchema = new mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: [true, "Account must be associated with a user"],
-        index: true
-    },
-    status: {
-        enum: {
-            values: ['ACTIVE', 'FROZEN', 'CLOSED'],
-            message: "Status must be either ACTIVE, FROZEN, or CLOSED"
+const accountSchema = new mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: [true, "Account must be associated with a user"],
+            index: true
+        },
+
+        status: {
+            type: String,
+            enum: {
+                values: ['ACTIVE', 'FROZEN', 'CLOSED'],
+                message: "Status must be either ACTIVE, FROZEN, or CLOSED"
+            },
+            default: 'ACTIVE'
+        },
+
+        currency: {
+            type: String,
+            required: [true, "Currency is required for creating an account"],
+            uppercase: true,
+            default: 'INR'
         }
     },
-    currency: {
-        type: String,
-        required: [true, "Currency is required for creating an account"],
-        uppercase: true,
-        default: 'INR'
-    }
+    { timestamps: true }
+);
 
-}, { timestamps: true });
-
-accountModel.index({ user: 1, status: 1 });
+// Compound index
+accountSchema.index({ user: 1, status: 1 });
 
 const accountModel = mongoose.model('Account', accountSchema);
 
