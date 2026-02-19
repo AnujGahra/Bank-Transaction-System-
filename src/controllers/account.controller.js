e78cffa923c6ccf11b3ed6a6387e39a35ef27368
Anuj Kumar <anujgahra244304@gmail.com>
2026-02-19 11:35:46 +0530

    Account Controller

--- src/controllers/account.controller.js
+++ src/controllers/account.controller.js
@@ -6,6 +6,16 @@ const accountModel = require('../models/account.model');
  */
 async function createAccount(req, res) {
 
+    const user = req.user;
+
+    const account = await accountModel.create({
+        user: user._id
+    })
+
+    res.status(201).json({
+        account
+    });
+
 
 }
 
