f1d5be7a34aee75bfb1b008cb64a388376374c24
Anuj Kumar <anujgahra244304@gmail.com>
2026-02-19 10:56:00 +0530

    Account Controller

---
+++ src/controllers/account.controller.js
@@ -0,0 +1,15 @@
+const accountModel = require('../models/account.model');
+
+/**
+ * - create account controller
+ * - POST /api/accounts/create
+ */
+async function createAccount(req, res) {
+
+
+}
+
+
+module.exports = {
+    createAccount
+};
