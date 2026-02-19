51c40b1fea9fa085e6aa3c58be15b1b6204e8e0e
Anuj Kumar <anujgahra244304@gmail.com>
2026-02-19 10:51:57 +0530

    Account Routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const authRouter = require('./routes/auth.routes');
 const cookieParser = require('cookie-parser');
+const authRouter = require('./routes/auth.routes');
+const accountRouter = require('./routes/account.routes');
 
 
 
@@ -9,7 +10,12 @@ const app = express();
 app.use(express.json());  // It is used to parse incoming JSON request bodies and make the data available in req.body.
 app.use(cookieParser());  // It is used to parse cookies from the incoming HTTP requests and make them available in req.cookies.
 
+/**
+ * - Use Routes
+
+ */
 app.use("/api/auth", authRouter);
+app.use("/api/accounts", accountRouter);
 
 
 module.exports = app;
